1546d2eadd84cfefd1b03e1ca9f596695b18a200
Famulski.Michal <187684@edu.p.lodz.pl>
2018-05-07 10:03:11 +0200

    linq

---
+++ ConsoleApplication1/ConsoleApplication4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = {"Ala", "Adam", "Tomek", "Piotr", "Ewa", "Kamil" };
+            var result = from name in names
+                         where name.Length < 5
+                         orderby name
+                         select name.ToUpper();
+            foreach(string item in result)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
