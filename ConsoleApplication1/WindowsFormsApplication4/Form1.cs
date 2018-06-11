fa15c2e228c20109a4357e7ff08288416f0688bb
Famulski.Michal <187684@edu.p.lodz.pl>
2018-06-11 09:21:45 +0200

    new project

---
+++ ConsoleApplication1/WindowsFormsApplication4/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Entity;
+
+namespace WindowsFormsApplication4
+{
+    public partial class Form1 : Form
+    {
+        NorthwindEntities db = new NorthwindEntities();
+        public Form1()
+        {
+            InitializeComponent();
+            //db.Customers.Load();
+            customersBindingSource.DataSource = db.Customers.ToList();
+            
+        }
+
+        private void customersBindingSource_CurrentChanged(object sender, EventArgs e)
+        {
+            var orders_customers = db.Orders.Where(s => s.CustomerID == ((Customers)customersBindingSource.Current).CustomerID);
+            ordersBindingSource.DataSource = orders_customers.ToList();
+        }
+
+        private void customersBindingNavigator_RefreshItems(object sender, EventArgs e)
+        {
+            customersBindingSource.EndEdit();
+            ordersBindingSource.EndEdit();
+            db.SaveChanges();
+        }
+    }
+}
